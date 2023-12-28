0b6b868d329aaa395bb72c1701f52a79b398a749
SARTHAK PARIKH <sarthakparikh@yahoo.com>
2023-12-27 19:08:27 -0500

    Added Formspree- Updated Layout - Add skills

--- js/index.js
+++ js/index.js
@@ -1,10 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const contactForm = document.getElementById('contactForm');
+document.addEventListener("DOMContentLoaded", function () {
+  const contactForm = document.getElementById("contactForm");
 
-    contactForm.addEventListener('submit', function (event) {
-        event.preventDefault();
-        // You can add code here to send an email using JavaScript
-        // For security reasons, you may need to use a server-side language (e.g., Node.js) to handle the email sending process.
-        alert('Form submitted! (Note: This is a placeholder, no actual submission is performed)');
-    });
+  contactForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(contactForm);
+    const xhr = new XMLHttpRequest();
+    // const url = "https://formspree.io/sarthakparikh@yahoo.com"; // Replace with your Formspree email
+    const url = "https://formspree.io/f/mnqeaela";
+    xhr.open("POST", url);
+    xhr.setRequestHeader("Accept", "application/json");
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          alert("Form submitted successfully!");
+          contactForm.reset();
+        } else {
+          alert("There was an error submitting the form. Please try again.");
+        }
+      }
+    };
+
+    xhr.send(formData);
+  });
 });
