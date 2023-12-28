dcdca2cbc3917bb6a9ea1759d2b13ca521fe612f
parikh-sarthak <154939140+parikh-sarthak@users.noreply.github.com>
2023-12-27 19:39:58 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,27 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const contactForm = document.getElementById("contactForm");
-
-  contactForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    const formData = new FormData(contactForm);
-    const xhr = new XMLHttpRequest();
-    // const url = "https://formspree.io/sarthakparikh@yahoo.com"; // Replace with your Formspree email
-    const url = "https://formspree.io/f/mnqeaela";
-    xhr.open("POST", url);
-    xhr.setRequestHeader("Accept", "application/json");
-
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-          alert("Form submitted successfully!");
-          contactForm.reset();
-        } else {
-          alert("There was an error submitting the form. Please try again.");
-        }
-      }
-    };
-
-    xhr.send(formData);
-  });
+    animateSkillBars();
 });
+
+function animateSkillBars() {
+    const skillBars = document.querySelectorAll(".skill-bar");
+    skillBars.forEach((bar) => {
+        const level = bar.dataset.level;
+        bar.style.width = `${level}%`;
+    });
+}
